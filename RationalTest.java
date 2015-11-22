3cf6a11ed1942d633de11ac35c6dcbae78372742
Ziyang Jiang <ziyangjiang@Ziyangs-MacBook-Pro.local>
2015-11-21 18:31:02 -0600

    Junit Test

--- RationalTest.java
+++ RationalTest.java
@@ -41,7 +41,74 @@ public class RationalTest extends TestCase {
         assertTrue( sRoot.isLessThan( HALF.plus( Rational.getTolerance() ) ) 
                         && HALF.minus( Rational.getTolerance() ).isLessThan( sRoot ) );
     }
+// fail
+    public void testPlus() {
+        Rational a = new Rational (-1,-2);
+        Rational b = new Rational (3,4);
+        Rational c = a.plus(b);
+        System.out.println("Test negative numbers: -1/-2 + 3/4 = "+ c.toString());
+        assertEquals(c.numerator(), 5);
+        assertEquals(c.denominator(), 4); 
+    }
 
+// fail
+    public void testMinus() {
+        Rational a = new Rational (-1, -4);   
+        Rational b = new Rational (3, 4);
+        Rational c = a.minus(b);
+        System.out.println("Test Minus: 1/4 - 3/4 = "+ c.toString());
+        assertEquals(c.numerator(), -1);
+        assertEquals(c.denominator(), 2);
+    }
+    public void testIsLessThan() {
+        Rational a = new Rational(1, 5);
+        Rational b = new Rational(3, 4);
+        assertTrue(a.isLessThan(b));
+    }
+
+    public void testTimes() {
+        Rational a = new Rational(3, 4);
+        Rational b = new Rational(2, 7);
+        Rational c = a.times(b);
+
+        System.out.println("Test times 3/4 * 2/7 = " + c.toString());
+        assertEquals(c.numerator(), 3);
+        assertEquals(c.denominator(), 14);
+
+    }
+
+    public void testDivides() {
+        Rational a = new Rational(3, 4);
+        Rational b = new Rational(2, 7);
+        Rational c = a.times(b);
+
+        System.out.println("Test divides 3/4 / 2/7 = " + c.toString());
+        assertEquals(c.numerator(), 21);
+        assertEquals(c.denominator(), 8);
+
+    }
+
+    public void testAbs() {
+        Rational a = new Rational(-1, -2);
+        Rational c = a.abs();
+        System.out.println("Abs of -1/-2 is "+ c.toString());
+        assertEquals(c.numerator(), 1);
+        assertEquals(c.denominator(), 2);
+
+    }
+
+    public void testTolerance() {
+        Rational a = new Rational(1, 300);
+        Rational.setTolerance(a);
+        assertEquals(Rational.getTolerance().numerator(), 1);
+        assertEquals(Rational.getTolerance().denominator(), 300);
+    }
+
+
+    public void testToString() {
+        Rational a = new Rational(1, 300);
+        assertEquals("1/300", a.toString());
+    }
     public static void main(String args[]) {
         String[] testCaseName = 
             { RationalTest.class.getName() };
